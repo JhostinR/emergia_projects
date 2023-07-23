8869ba9cc6bddaf34a81c0a44bbf92c4ebaaf5c9
Cristian Patiño Ospina <cristianpatinoospina45@gmail.com>
2023-07-23 11:40:20 -0500

    .

--- descifrador/main.php
+++ descifrador/main.php
@@ -3,50 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>Verificar Archivo</title>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
-    <h1>Añade tu archivo</h1>
-    <form class="submit" action="" method="post" enctype="multipart/form-data">
-        <input class="file" type="file" name="archivo" id="archivo" value="Seleccionar Archivo">
-        <input class="submit" type="submit" name="submit" value="Subir Archivo">
+    <h1>AÑADE TU ARCHIVO</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <!-- Botón para seleccionar el archivo -->
+        <input type="file" name="archivo" id="archivo">
+        <input class="submit" type="submit" value="Subir archivo" name="submit">
     </form>
 
     <?php
-    function verificarArchivoSinExtension($ruta, $nombreArchivo)
-    {
-        $nombreCortado = substr($nombreArchivo, 0, -4);
-        $archivoSinExtension = $ruta . DIRECTORY_SEPARATOR . $nombreCortado;
-        return file_exists($archivoSinExtension);
-    }
-
-    if (isset($_FILES['archivo'])) {
+    if (isset($_POST['submit'])) {
+        // Obtener el nombre del archivo seleccionado
         $nombreArchivo = $_FILES['archivo']['name'];
-        $rutaArchivo = $_FILES['archivo']['tmp_name'];
+        $nombreTempArchivo = $_FILES['archivo']['tmp_name'];
 
-        // Carpeta donde se guardarán los archivos
-        $carpetaDestino = 'ruta/donde/guardar';
+        // Ruta donde se guardará el archivo (directorio absoluto)
+        $directorioDestino = __DIR__ . '/';
 
-        // Movemos el archivo subido a la carpeta destino
-        move_uploaded_file($rutaArchivo, $carpetaDestino . DIRECTORY_SEPARATOR . $nombreArchivo);
+        // Mover el archivo a la ubicación deseada (no necesario para este caso)
+        // move_uploaded_file($nombreTempArchivo, $directorioDestino . $nombreArchivo);
 
-        // Mostramos la ruta del archivo seleccionado
-        echo '<p>Ruta del archivo seleccionado: ' . $carpetaDestino . DIRECTORY_SEPARATOR . $nombreArchivo . '</p>';
+        // Quitar la extensión .pgp del nombre del archivo
+        $nombreCortado = substr($nombreArchivo, 0, -4);
 
-        // Verificamos si existe el archivo sin extensión
-        $resultado = verificarArchivoSinExtension($carpetaDestino, $nombreArchivo);
+        // Renombrar el archivo sin la extensión .pgp
+        $nuevoNombreArchivo = $directorioDestino . $nombreCortado;
 
-        if ($resultado) {
-            echo '<p>El archivo sin extensión existe en la misma ruta.</p>';
+        if (rename($nombreTempArchivo, $nuevoNombreArchivo)) {
+            echo "<p>El archivo se ha subido y renombrado correctamente.</p>";
+
+            // Verificar si el archivo cortado existe en la misma ruta
+            if (file_exists($nuevoNombreArchivo)) {
+                echo "<p>El archivo sin extensión .pgp existe en la misma ruta.</p>";
+            } else {
+                echo "<p>El archivo sin extensión .pgp no existe en la misma ruta.</p>";
+            }
         } else {
-            echo '<p>El archivo sin extensión NO existe en la misma ruta.</p>';
+            echo "<p>Error al subir y renombrar el archivo.</p>";
         }
     }
     ?>
-
-    <!-- <form class="carpeta" action="ruta/carpeta/archivos/windows" method="post">
-        <input type="submit" name="carpeta_windows" value="Ir a la carpeta de archivos de Windows">
-    </form> -->
 </body>
 </html>
