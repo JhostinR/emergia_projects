df294b07441ac5bbc47cf9ca510d17038ba6b44d
Cristian Patiño Ospina <cristianpatinoospina45@gmail.com>
2023-09-11 08:57:08 -0500

    .

--- dividir_unir_pdf/view/form_dividir.py
+++ dividir_unir_pdf/view/form_dividir.py
@@ -11,44 +11,57 @@ class PDFDividerApp:
         self.ventana_principal = ventana_principal  
         self.ventana = tk.Toplevel(self.ventana_principal)  
         self.ventana.title('Dividir PDFs')
+        self.ventana.config(bg='#ffd6dc')
         self.ventana.geometry("500x350")
         self.ventana.resizable(False, False)
         help.centerWindows(self.ventana,350,500) # height width
         self.ventana.protocol("WM_DELETE_WINDOW", self.on_close)  # Captura el evento de cierre
 
-        self.entry_filename = tk.Entry(self.ventana, width=40)
-        self.entry_filename.grid(row=0, column=0, padx=10, pady=10)
+        # Titulo principal
+        titulo = tk.Label(self.ventana, text="Dividir PDF", font=("Arial", 20, "bold"), foreground="#f5425d", bg="#ffd6dc")
+        titulo.place(x=170,y=8)
+
+        self.entry_filename = tk.Entry(self.ventana, width=24)
+        self.entry_filename.place_forget()
 
         # Botón para seleccionar el archivo PDF
-        self.select_file_button = tk.Button(self.ventana, text="Seleccionar PDF", command=self.select_pdf_file)
-        self.select_file_button.grid(row=0, column=1, padx=10, pady=10)
+        self.select_file_button = tk.Button(self.ventana, text="Seleccionar archivo", font=("Arial", 10, "bold"), command=self.select_pdf_file, width=18, bg='#ff8a9a')
+        self.select_file_button.bind('<Enter>', lambda e: e.widget.config(bg='#f7072b'))
+        self.select_file_button.bind('<Leave>', lambda e: e.widget.config(bg='#ff8a9a'))
+        self.select_file_button.place(x=180, y=70)
 
         # Etiqueta para mostrar la ruta del archivo PDF seleccionado
-        self.selected_file_label = tk.Label(self.ventana, text="Ruta del archivo PDF: ")
-        self.selected_file_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+        self.selected_file_label = tk.Label(self.ventana, text="Ruta del archivo PDF: ", font=("Arial", 8, "bold"), bg='#ffd6dc')
+        self.selected_file_label.place(x=40, y=110)
+
+        # Etiqueta para la instrucción
+        self.num_pages = tk.Label(self.ventana, text="Número de páginas por PDF:", font=("Arial", 10, "bold"), bg='#ffd6dc')
+        self.num_pages.place(x=70, y=150)
 
         # Cuadro de entrada para el número de páginas por PDF
         self.entry_pages_per_pdf = tk.Entry(self.ventana, width=10)
-        self.entry_pages_per_pdf.grid(row=2, column=0, padx=10, pady=10)
-
-        # Etiqueta para la instrucción
-        self.num_pages = tk.Label(self.ventana, text="Número de páginas por PDF")
-        self.num_pages.grid(row=2, column=1, padx=10, pady=10)
+        self.entry_pages_per_pdf.place(x=270, y=150)
 
         # Botón para seleccionar la ubicación de guardado
-        self.select_output_button = tk.Button(self.ventana, text="Seleccionar Carpeta de Salida", command=self.select_output_folder)
-        self.select_output_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        self.select_output_button = tk.Button(self.ventana, text="Seleccionar Carpeta de Salida", font=("Arial", 10, "bold"), command=self.select_output_folder, width=24, bg='#ff8a9a')
+        self.select_output_button.bind('<Enter>', lambda e: e.widget.config(bg='#f7072b'))
+        self.select_output_button.bind('<Leave>', lambda e: e.widget.config(bg='#ff8a9a'))
+        self.select_output_button.place(x=160, y=190)
 
         # Etiqueta para mostrar la ruta de la carpeta de salida
-        self.output_label = tk.Label(self.ventana, text="Ruta de salida: ")
-        self.output_label.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+        self.output_label = tk.Label(self.ventana, text="Ruta de salida: ", font=("Arial", 8, "bold"), bg='#ffd6dc')
+        self.output_label.place(x=60, y=230)
 
         # Botón para dividir el archivo PDF
-        self.divide_button = tk.Button(self.ventana, text="Dividir PDF", command=self.divide_pdf)
-        self.divide_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+        self.divide_button = tk.Button(self.ventana, text="Dividir PDF", font=("Arial", 10, "bold"), command=self.divide_pdf, width=11, bg='#ff8a9a')
+        self.divide_button.bind('<Enter>', lambda e: e.widget.config(bg='#f7072b'))
+        self.divide_button.bind('<Leave>', lambda e: e.widget.config(bg='#ff8a9a'))
+        self.divide_button.place(x=200, y=270)
 
-        self.close_button = tk.Button(self.ventana, text="cerrar", command=self.on_close)
-        self.close_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
+        self.close_button = tk.Button(self.ventana, text="Cerrar", font=("Arial", 10, "bold"), command=self.on_close, width=8, bg='#ff8a9a')
+        self.close_button.bind('<Enter>', lambda e: e.widget.config(bg='#f7072b'))
+        self.close_button.bind('<Leave>', lambda e: e.widget.config(bg='#ff8a9a'))
+        self.close_button.place(x=410, y=310)
         
         self.ventana.mainloop()
 
