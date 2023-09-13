ed7b57a7fd3f2a44337a2c232974ced0516d6890
Cristian Patiño Ospina <cristianpatinoospina45@gmail.com>
2023-09-13 12:01:58 -0500

    .

--- metodos_excel/view/form_principal.py
+++ metodos_excel/view/form_principal.py
@@ -31,7 +31,7 @@ class Visualizador:
         label.config(image=imagen_tk, bg="#fff")
         
         # Titulo principal
-        titulo = tk.Label(self.ventana_principal, text="Dιʋιԃιɾ PDF", font=("Arial", 26, "bold"), foreground="#fff", bg="#fff")
+        titulo = tk.Label(self.ventana_principal, text="Dιʋιԃιɾ PDF", font=("Arial", 26, "bold"), foreground="#000", bg="#ffd6dc")
         titulo.place(x=150,y=8)
 
         self.entry_filename = tk.Entry(self.ventana_principal, width=24)
@@ -46,23 +46,27 @@ class Visualizador:
         self.selected_file_label = tk.Label(self.ventana_principal, text="Ruta del archivo PDF: ", font=("Arial", 8, "bold"), bg='#ffd6dc')
         self.selected_file_label.place(x=20, y=110)
 
-        self.label_rows = tk.Label(self.ventana_principal, text="pruebas", font=("Arial", 10, "normal"), bg='#fff')
+        self.label_rows = tk.Label(self.ventana_principal, text="pruebas", font=("Arial", 10, "normal"), bg='#ffd6dc')
         self.label_rows.place(x=20, y=150)
         
         self.ventana_principal.mainloop()
 
     def select_pdf_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("xlsx files", "*.xlsx")])
+        file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv"), ("Excel files", "*.xlsx")])
         self.entry_filename.delete(0, tk.END)
         self.entry_filename.insert(0, file_path)
         self.selected_file_label.config(text=f"Ruta del archivo PDF: {file_path}")
 
-        # Read the Excel file
-        df = pd.read_excel(file_path)
+        if file_path.endswith(".csv"):
+            df = pd.read_csv(file_path)
+        elif file_path.endswith(".xlsx"):
+            df = pd.read_excel(file_path)
+        else:
+            df = pd.read_csv(file_path)
 
         # Print the first 5 rows
         # Add a new label to the GUI window
-        label_rows = tk.Label(self.ventana_principal, text=df.head(5).to_string(), font=("Arial", 10, "normal"), bg='#fff')
+        label_rows = tk.Label(self.ventana_principal, text=df.head(5).to_string(), font=("Arial", 10, "normal"), bg='#ffd6dc')
         label_rows.place(x=20, y=150)
 
 if __name__ == "__main__":
