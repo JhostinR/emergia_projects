c07899c170dbdbd47f6dd9c9448c2436516e7946
Cristian Patiño Ospina <cristianpatinoospina45@gmail.com>
2023-09-20 10:38:34 -0500

    Update form_principal.py

--- metodos_excel/view/form_principal.py
+++ metodos_excel/view/form_principal.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 import pandas as pd
 import shutil
 from shutil import move
-from os import path, listdir, rename, makedirs
+from os import path, listdir, rename, makedirs, walk
 
 help = Helpers()
 
@@ -86,8 +86,8 @@ class Visualizador:
         self.select_folder_button.bind('<Leave>', lambda e: e.widget.config(bg='#b1fac0'))
         self.select_folder_button.place(x=230, y=400)
         
-        self.select_validar_button = tk.Button(self.ventana_principal, text="Validar 2.0", font=("Arial", 10, "bold"), command=exit, width=18, bg='#b1fac0')
-        self.select_validar_button.bind('<Enter>', lambda e: e.widget.config(bg='#016615'))
+        self.select_validar_button = tk.Button(self.ventana_principal, text="Validar 2.0", font=("Arial", 10, "bold"), command=self.validate_files_2, width=18, bg='#b1fac0')
+        self.select_validar_button.bind('<Enter>', lambda e: e.widget.config(bg='#016615')) 
         self.select_validar_button.bind('<Leave>', lambda e: e.widget.config(bg='#b1fac0'))
         self.select_validar_button.place(x=230, y=450)
         
@@ -409,6 +409,49 @@ class Visualizador:
         messagebox.showinfo("carpetas movidas", "las carpetas se ham movido")
 #endregion move folder
 # ---------------------------------------------------------------------------------------------------------------------
+#region validar 2.0
+    def validate_files_2(self):
+        """Crea un archivo CSV con las carpetas y los archivos que contienen. Cada fila del archivo CSV contiene la siguiente información:
+
+        * Nombre de la carpeta
+        * Lista de archivos que contiene la carpeta
+
+        Args:
+            self (Visualizador): Instancia de la clase Visualizador.
+        """
+
+        # Obtener la ruta de la carpeta seleccionada
+        selected_folder_path = self.entry_foldername.get()
+
+        # Validar que la carpeta exista
+        if not selected_folder_path:
+            messagebox.showerror("Error", "¡Selecciona una carpeta!")
+            return
+
+        # Crear una lista para almacenar las carpetas y archivos
+        carpetas_archivos = []
+
+        # Iterar a través de las subcarpetas de la carpeta seleccionada
+        for subfolder_path, _, filenames in walk(selected_folder_path):
+            # Obtener el nombre de la carpeta
+            folder_name = path.basename(subfolder_path)
+
+            # Crear una lista de archivos que contiene la carpeta
+            file_names = [f for f in filenames if path.isfile(path.join(subfolder_path, f))]
+
+            # Agregar la carpeta y los archivos a la lista
+            carpetas_archivos.append({
+                "Carpeta": folder_name,
+                "Archivos": ",".join(file_names)
+            })
+
+        # Guardar la lista de carpetas y archivos en un archivo CSV
+        df_carpetas_archivos = pd.DataFrame.from_dict(carpetas_archivos)
+        df_carpetas_archivos.to_csv("carpetas_archivos.csv", index=False)
+
+        messagebox.showinfo("¡Éxito!", "Se ha creado el archivo CSV con las carpetas y archivos.")
+        
+#endregion validar 2.0
 #region close
     def close(self):
         self.ventana_principal.destroy()
